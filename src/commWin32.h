a6eaafa4ef099d1ee77fbfc6d88a4a634ae21552
Marwan <marwansanehaji@gmail.com>
2023-11-17 17:31:14 +0100

    main

--- src/commWin32.h
+++ src/commWin32.h
@@ -1,7 +1,15 @@
-#include <>
+#include <cstdio>
+#include <sec_api/stdio_s.h>
+#include <windows.h>
+#include <winbase.h>
+#include <minwindef.h>
+#include <winerror.h>
 
+
+void PrintPortState(DCB dcb);
 int GetComList(int* ports,int size,int* numofports);
-int SerialAttach(int portNum);
+int OpenPort(int portNum);
+int ClosePort();
 
 int SWrite(char* buf,int size,long unsigned int* bytesWritten);
 int SRead(char* buf,int size,long unsigned int* bytesRead);
